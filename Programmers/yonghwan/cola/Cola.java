460d49a4a81dc2f6fdd29862674b9ad0a9923fac
YongLeeCode <yonghwan311@gmail.com>
2024-11-28 14:29:50 +0900

    feat: 콜라 문제 구현

--- Programmers/yonghwan/cola/Cola.java
+++ Programmers/yonghwan/cola/Cola.java
@@ -1,5 +1,15 @@
-public class Example{
-    public static void main(String[] args) {
-        System.out.println("Hello, World")
+public class Cola {
+    static int newColaCount = 0;
+
+    public static int solveProblem(int a, int b, int n) {
+        int newCola = n / a * b;
+        int emptyBottle = n % a + n / a * b;
+
+        newColaCount += newCola;
+
+        if(n / a > 0){
+            solveProblem(a, b, emptyBottle);
+        }
+        return newColaCount;
     }
 }
