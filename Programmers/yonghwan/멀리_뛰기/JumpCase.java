69c1ce96ec4199529fb601254440a71be824cc66
YongLeeCode <yonghwan311@gmail.com>
2025-01-15 19:49:49 +0900

    feat: 11 회차 문제 구현
    - 게임맵 최단거리: bfs
    - 멀리 뛰기: dp
    - 큰 수 만들기: greedy

--- Programmers/yonghwan/멀리_뛰기/JumpCase.java
+++ Programmers/yonghwan/멀리_뛰기/JumpCase.java
@@ -1,4 +1,21 @@
 package 멀리_뛰기;
 
 public class JumpCase {
+    public static long solution(int n) {
+        long[] answer = new long[2000];
+        answer[1] = 1;
+        answer[2] = 2;
+
+        for(int i = 3; i < answer.length; i++) {
+            answer[i] = answer[i - 1] + answer[i - 2] % 1234567;
+        }
+
+        return answer[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        long result = solution(n);
+        System.out.println(result);
+    }
 }
