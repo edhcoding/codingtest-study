6736c35fc3ad87e464f52e5d30ac9b6ba4cf4608
eundonghyeok <mephisto7191@naver.com>
2025-12-08 22:48:12 +0900

    Lv3 정수 삼각형 리펙토링

--- Programmers/donghyeok/Lv3/정수 삼각형.js
+++ Programmers/donghyeok/Lv3/정수 삼각형.js
@@ -2,25 +2,19 @@
 
 // 문제: 삼각형의 꼭대기부터 바닥까지 내려가면서 합이 최대가 되는 경로를 찾기
 function solution(triangle) {
-  // 원본 복사 - 각 위치에서 최대 합 저장하려고
-  const dp = triangle.map((row) => [...row]);
-
-  for (let i = 1; i < triangle.length; i++) {
-    for (let j = 0; j < triangle[i].length; j++) {
-      if (j === 0) {
-        dp[i][j] += dp[i - 1][0];
-      }
-      else if (j === triangle[i].length - 1) {
-        dp[i][j] += dp[i - 1][j - 1];
-      }
-      else {
-        dp[i][j] += Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
-      }
-    }
-  }
-
-  const lastRow = dp[dp.length - 1];
-  return Math.max(...lastRow);
+  return Math.max(
+    ...triangle.reduce((cost, line) => {
+      return line.map((v, index) => {
+        return (
+          v + // 현재 값
+          Math.max(
+            index < cost.length ? cost[index] : 0, // cost 왼쪽 값
+            index > 0 ? cost[index - 1] : 0 // cost 오른쪽 값
+          )
+        );
+      });
+    }, [])
+  );
 }
 
 console.log(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]])); // 30
