ecf5224409987d0030acbcbcb5055f4f1badb2c7
eundonghyeok <mephisto7191@naver.com>
2025-12-31 23:55:50 +0900

    Lv1 내적2

--- Programmers/donghyeok/Lv1/내적.js
+++ Programmers/donghyeok/Lv1/내적.js
@@ -3,8 +3,24 @@
 // 문제:  두 1차원 정수 배열 a, b가 매개변수로 주어집니다. a와 b의 내적을 return 하도록 solution 함수를 완성
 function solution(a, b) {
   // reduce를 사용하여 각 요소를 곱한 후 모두 더하기
+  console.log(
+    a.reduce((sum, value, index) => {
+      console.log(sum, value, index);
+      return sum + value * b[index];
+    }, 0)
+  );
   return a.reduce((sum, value, index) => sum + value * b[index], 0);
 }
 
 console.log(solution([1, 2, 3, 4], [-3, -1, 0, 2])); // 3
 console.log(solution([-1, 0, 1], [1, 0, -1])); // -2
+
+function solution2(a, b) {
+  let answer = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    answer += a[i] * b[i];
+  }
+
+  return answer;
+}
