35bf202040fa5121c12488f1ad79b41006f5f1c7
eundonghyeok <mephisto7191@naver.com>
2026-01-25 22:50:07 +0900

    Lv1 문자열 다루기 기본

--- Programmers/donghyeok/Lv2/124 나라의 숫자.js
+++ Programmers/donghyeok/Lv2/124 나라의 숫자.js
@@ -1,12 +1,12 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12899
 
 function solution(n) {
-  let answer = '',
-  numbers = ['4', '1', '2']
+  let answer = "",
+    numbers = ["4", "1", "2"];
 
-  while(n){
-    answer = numbers[n % 3] + answer
-    n = n % 3 == 0 ? n / 3 - 1 : Math.floor(n / 3)
+  while (n) {
+    answer = numbers[n % 3] + answer;
+    n = n % 3 == 0 ? n / 3 - 1 : Math.floor(n / 3);
   }
 
   return answer;
