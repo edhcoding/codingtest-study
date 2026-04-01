8175f99683227ea5f9089a7004213504c269410c
eundonghyeok <mephisto7191@naver.com>
2026-04-01 23:29:39 +0900

    Lv3 등굣길

---
+++ Programmers/donghyeok/Lv3/등굣길.js
@@ -0,0 +1,41 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42898
+
+function solution(m, n, puddles) {
+  const dp = Array.from(Array(n), () => Array(m).fill(0));
+  puddles.forEach(([x, y]) => (dp[y - 1][x - 1] = -1));
+
+  dp[0][0] = 1;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      let path = dp[i][j];
+      if (path === -1) continue;
+
+      if (i - 1 >= 0 && dp[i - 1][j] !== -1) path += dp[i - 1][j];
+      if (j - 1 >= 0 && dp[i][j - 1] !== -1) path += dp[i][j - 1];
+      dp[i][j] = path % 1000000007;
+    }
+  }
+  return dp[n - 1][m - 1] % 1000000007;
+}
+
+// 다른 사람 풀이
+function solution(M, N, puddles) {
+  const T = Array(M)
+    .fill(null)
+    .map(() => []);
+  const chk = new Set(puddles.map(([x, y]) => `${x - 1}:${y - 1}`));
+
+  for (let i = 0; i < M; i++) {
+    for (let j = 0; j < N; j++) {
+      if (chk.has(`${i}:${j}`)) T[i][j] = 0;
+      else if (i === 0 && j === 0) T[i][j] = 1;
+      else if (i === 0) T[i][j] = T[i][j - 1];
+      else if (j === 0) T[i][j] = T[i - 1][j];
+      else T[i][j] = (T[i - 1][j] + T[i][j - 1]) % 1000000007;
+    }
+  }
+  return T[M - 1][N - 1];
+}
+
+console.log(solution(4, 3, [[2, 2]])); // 4
